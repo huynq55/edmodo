304035d84c9c9321884ce1be53aaf1268f811d83
huynq55 <quanghuy_2309@yahoo.com.vn>
2012-02-28 09:44:38 +0700

    add application

--- settings.py
+++ settings.py
@@ -115,6 +115,7 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     'django.contrib.admindocs',
+    '.library',
 )
 
 # A sample logging configuration. The only tangible logging
