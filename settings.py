5c6e4e54f72b9b37d98eff67fd1a32c92f5a06e9
Quang Huy <quanghuy_2309@yahoo.com.vn>
2012-02-28 08:14:00 +0700

    enable django admin

--- settings.py
+++ settings.py
@@ -112,9 +112,9 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
+    'django.contrib.admindocs',
     'EdmodoLibrary.Library',
 )
 
