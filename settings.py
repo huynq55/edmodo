df2e9fb0a29977a534dfd5004926f2ce2dd2eb8b
Quang Huy <quanghuy_2309@yahoo.com.vn>
2012-03-09 19:17:37 +0700

    thay doi file base va nav

--- settings.py
+++ settings.py
@@ -14,8 +14,8 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'libdb',                      # Or path to database file if using sqlite3.
         'USER': 'root',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PASSWORD': 'minhhoang',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
@@ -102,7 +102,7 @@ MIDDLEWARE_CLASSES = (
 
 ROOT_URLCONF = 'edmodo.urls'
 
-TEMPLATE_DIRS = ('E:/Project/edmodo//templates',)
+TEMPLATE_DIRS = ('D:/edmodo//templates',)
 
 INSTALLED_APPS = (
     'django.contrib.auth',
