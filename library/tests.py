f14ae912bb5bee83e908b28aa4080401699833fa
Do Duc Kien <hunken92@gmail.com>
2012-05-17 23:27:19 +0700

    Test with selenium va test.py

--- library/tests.py
+++ library/tests.py
@@ -1,16 +1,70 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
+from django.contrib.auth.models import User
 
+class LibraryTest(TestCase):
+    def test_register_page(self):
+        data={
+            'username':'admin',
+            'email':'email@gmail.com',
+            'password1':'123456',
+            'password2':'123456',
+        }
+        response = self.client.post('/register/',data)
+        self.assertContains(response,"Register")
+    def test_login(self):
+        response = self.client.post('/accounts/login/',{
+            'usename' : 'admin',
+            'password' : 'admin',
+        })
+        self.assertEqual(response.status_code, 200)
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+    def test_upload_book(self):
+        response = self.client.post('/accounts/login/',{
+            'usename' : 'admin',
+            'password' : 'admin',
+            })
+        self.assertEqual(response.status_code, 200)
+        self.client.post('/upload/book/',{
+            'title': 'Test',
+            'up_file':'E:\RELAX\hehe.pdf',
+            'description':'test',
+        })
+        self.assertEqual(response.status_code, 200)
+    def test_upload_image(self):
+        response = self.client.post('/accounts/login/',{
+            'usename' : 'admin',
+            'password' : 'admin',
+            })
+        self.assertEqual(response.status_code, 200)
+        self.client.post('/upload/image/',{
+            'title': 'Test',
+            'up_file':'E:\RELAX\hehe.jpg',
+            'description':'test',
+            })
+        self.assertEqual(response.status_code, 200)
+    def test_add_image_link(self):
+        response = self.client.post('/accounts/login/',{
+            'usename' : 'admin',
+            'password' : 'admin',
+            })
+        self.assertEqual(response.status_code, 200)
+        self.client.post('/upload/image_link/',{
+            'title': 'Test',
+            'link':'http://cyears.files.wordpress.com/2011/04/image4.jpg',
+            'description':'test',
+            })
+        self.assertEqual(response.status_code, 200)
+
+    def test_add_video_link(self):
+        response = self.client.post('/accounts/login/',{
+            'usename' : 'admin',
+            'password' : 'admin',
+            })
+        self.assertEqual(response.status_code, 200)
+        self.client.post('/upload/video_link/',{
+            'title': 'Test',
+            'link':'http://youtu.be/IQJXFSAn-es',
+            'description':'test',
+            })
+        self.assertEqual(response.status_code, 200)
+
